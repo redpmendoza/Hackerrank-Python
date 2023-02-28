747b481d12768e050cc1aab3d5dc26f5cfdbc898
Rhabi Mendoza <111347175+redpmendoza@users.noreply.github.com>
2023-02-28 17:23:06 +0800

    Add files via upload

---
+++ WhatsYourName.py
@@ -0,0 +1,10 @@
+# Function print full name
+def print_full_name(first, last):
+    print("Hello {} {}! You just delved into python.".format(first, last))
+
+# Get first and last
+first = input()
+last = input()
+
+# Output string
+print_full_name(first, last)
